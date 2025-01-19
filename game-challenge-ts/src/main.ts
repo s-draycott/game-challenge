952a51926fd43ac321f6c3dfdda2873b0f501acd
unknown <sally-draycott@hotmail.co.uk>
2025-01-19 00:22:47 +0000

    started timer function

--- game-challenge-ts/src/main.ts
+++ game-challenge-ts/src/main.ts
@@ -1,3 +1,24 @@
 import '../stylesheet.scss';
+const timer = document.getElementById("timer");
 
+
+const paragraphs = [
+    "A long time ago in a galaxy far, far away... Rebel spaceships, battered and few, are manoeuvring a desperate last stand against the mighty Imperial Starfleet. An ancient weapon, the Death Star, capable of destroying a planet, is now fully operational and under construction. The fate of the rebellion lies with a small band of brave fighters who have only one chance to strike from the shadows."
+]
+
+if(!timer) {
+    throw new Error("Some elements can not be found")
+}
+
+
+
+
+
+let timerLength = 60;
+const timerFunc = () => {
+    if (timerLength > 0) {
+        timerLength--;
+        timer.innerHTML = " " + timerLength;
+    } // else display the score
+}; setInterval(timerFunc, 1000);
 
